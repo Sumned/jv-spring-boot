39b21072057ce481b6020cd1dadd36004b389a40
Sumned <vladar91@gmail.com>
2020-06-24 13:14:06 +0300

    initialization

---
+++ src/main/java/com/dev/springboot/JvSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvSpringBootApplication.class, args);
+    }
+
+}
