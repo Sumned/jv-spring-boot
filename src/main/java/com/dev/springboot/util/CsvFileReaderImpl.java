71c7bf985054da9a7102c2ea81e0e72cdfd7d937
Sumned <vladar91@gmail.com>
2020-07-10 16:08:22 +0300

    fix2

--- src/main/java/com/dev/springboot/util/CsvFileReaderImpl.java
+++ src/main/java/com/dev/springboot/util/CsvFileReaderImpl.java
@@ -7,15 +7,13 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import lombok.Cleanup;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CsvFileReaderImpl implements CsvFileReader {
 
     public List<String> readFile(String path) {
-        try {
-            @Cleanup Stream<String> stringStream = Files.lines(Paths.get(path));
+        try (Stream<String> stringStream = Files.lines(Paths.get(path))) {
             return stringStream.collect(Collectors.toList());
         } catch (IOException e) {
             throw new UndeclaredThrowableException(e);
